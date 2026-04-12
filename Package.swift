d320cb71183b23f71647d4773bf613fb2c083459
UnityLevelPlay <svc-levelplay-release@unity3d.com>
2026-04-12 07:12:53 +0000

    Release 5.11.0 - SDK 8.1.1

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     .library(name: "MintegralAdapter", targets: ["MintegralAdapter"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/Mintegral-official/MintegralAdSDK-Swift-Package", exact: "8.1.0"),
+    .package(url: "https://github.com/Mintegral-official/MintegralAdSDK-Swift-Package", exact: "8.1.1"),
     .package(url: "https://github.com/ironsource-mobile/Unity-Mediation-iAds-Swift-Package", "9.2.0"..<"10.0.0"),
   ],
   targets: [
@@ -22,8 +22,8 @@ let package = Package(
     ),
     .binaryTarget(
       name: "MintegralAdapterSDK",
-      url: "https://raw.githubusercontent.com/ironsource-mobile/iOS-adapters/master/mintegral-adapter/5.10.0/ISMintegralAdapter5.10.0.zip",
-      checksum: "b52410957f4e527149b66426ec1e6b3bcd6b30ff6aa6fbe2967c996a67bc5444"
+      url: "https://raw.githubusercontent.com/ironsource-mobile/iOS-adapters/master/mintegral-adapter/5.11.0/ISMintegralAdapter5.11.0.zip",
+      checksum: "509b0dea8b169837702dfe779e1c81610b5b57c3e04dc8c9471d690f3cf59180"
     )
   ]
 )
